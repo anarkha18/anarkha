e0c1d039ddadf3c09555f953c75eabccd097a168
anargha <anarghasanon04@gmail.com>
2021-02-10 15:11:06 +0530

    1-4 asgmnt

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('asgmnt1', views.assignment1, name="asgmnt1"),
+    path('asgmnt2', views.assignment2, name="asgmnt2"),
+    path('asgmnt3', views.assignment3, name="asgmnt3"),
+    path('asgmnt4', views.assignment4, name="asgmnt4"),
+]
---
+++ home/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def assignment1(request):
+    return render(request, "assignment1.html")
+def assignment2(request):
+    return render(request, "assignment2.html")
+def assignment3(request):
+    return render(request, "assignment3.html")
+def assignment4(request):
+    return render(request, "assignment4.html")
